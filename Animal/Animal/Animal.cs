942500337bc559a3cc5b02c8de9ffd3bd3319d56
FiveEYZ <b-d-t@msn.com>
2016-10-31 11:53:44 +0100

    Init commit

---
+++ Animal/Animal/Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Animal
+    {
+        private string _name { get; set; }
+        private bool _haveTail { get; set; }
+        private bool _isDangerous { get; set; }
+
+        public Animal(string Name, bool HaveTail, bool IsDangerous)
+        {
+            Name = _name;
+            HaveTail = _haveTail;
+            IsDangerous = _isDangerous;
+        }
+        public string AnimalName()
+        {
+            return "Animal; " + _name + "have " + _haveTail + "and is " + _isDangerous;
+        }
+    }
+}
